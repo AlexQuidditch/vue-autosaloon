bc412fdff5a2d9c085d1bdd51871ca1c9565b913
AlexQuidditch <alexquidditch@gmail.com>
2017-03-21 00:29:34 +0700

    added plugins into main.js

--- src/main.js
+++ src/main.js
@@ -1,13 +1,26 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
+import Vue from 'vue';
+import router from './router';
+import { store, intro } from './store/store';
+
+import App from './App';
+
+import VueResource from 'vue-resource';
+import ScrollTo from 'vue-scrollto';
+
+Vue.use(VueResource);
+Vue.use(ScrollTo);
+
+Vue.http.options.root = 'https://autosaloon-293cd.firebaseio.com/'
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#wrapper',
-  router,
-  template: '<App/>',
-  components: { App }
+	el: '#wrapper',
+	router,
+	store,
+	template: '<App/>',
+	components: {
+		App
+	}
 })
