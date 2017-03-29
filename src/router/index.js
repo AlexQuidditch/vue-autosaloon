b78333e74760307e5ecf7af3a817aeb55cc31fc6
AlexQuidditch <alexquidditch@gmail.com>
2017-03-29 16:32:58 +0700

    added new routes

--- src/router/index.js
+++ src/router/index.js
@@ -4,37 +4,94 @@ import hello from '@/components/hello'
 import main from '@/components/main'
 import services from '@/components/services'
 import catalogue from '@/components/catalogue'
-import about from '@/components/about'
+import all from '@/components/catalogue/filter/all'
+import chery from '@/components/catalogue/filter/chery'
+import changan from '@/components/catalogue/filter/changan'
+import hawtai from '@/components/catalogue/filter/hawtai'
+import about from '@/components/about';
+
+import tigga3 from '@/components/catalogue/chery/tigga3';
 
 Vue.use(Router)
 
+const scrollBehavior = (to, from, savedPosition) => {
+	if (savedPosition) {
+		return savedPosition
+	} else {
+		const position = {};
+		if (to.hash) {
+			position.selector = to.hash
+		}
+		if (to.matched.some(m => m.meta.scrollToTop)) {
+ 			position.x = 0
+ 			position.y = 0
+		}
+		return position
+	}
+};
+
 export default new Router({
 	mode: 'history',
+	base: __dirname,
+	scrollBehavior,
 	linkActiveClass: '_active',
 	routes: [
 		{
+			name: 'main',
 			path: '/',
-			name: 'Main',
 			component: main
 		},
 		{
+			name: 'services',
 			path: '/services',
-			name: 'Services',
 			component: services
 		},
 		{
+			name: 'catalogue',
 			path: '/catalogue',
-			name: 'Catalogue',
-			component: catalogue
+			component: catalogue,
 		},
 		{
+			name: 'tigga3',
+			path: '/catalogue/tigga3',
+			component: tigga3
+		},
+		{
+			name: 'all',
+			path: '/all',
+			components: {
+				a: all
+			}
+		},
+		{
+			name: 'chery',
+			path: '/chery',
+			components: {
+				c: chery
+			}
+		},
+		{
+			name: 'hawtai',
+			path: '/hawtai',
+			components: {
+				h: hawtai
+			}
+		},
+		{
+			name: 'changan',
+			path: '/changan',
+			components: {
+				ch: changan
+			}
+		},
+		{
+			name: 'about',
 			path: '/about',
-			name: 'About',
 			component: about
 		},
 		{
+			name: 'hello',
 			path: '/hello',
-			name: 'Hello',
 			component: hello
 		}
 	]
